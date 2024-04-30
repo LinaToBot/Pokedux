da7a38fb8ba4d1123aec424b07da41356363c32b
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-04-30 10:47:52 -0600

    just pokemons

--- src/reducers/pokemon.js
+++ src/reducers/pokemon.js
@@ -1,20 +1,16 @@
 import { fromJS, get, getIn, setIn } from "immutable";
-import { SET_POKEMONS, SET_LOADING, SET_FAVORITE } from "../actions/types";
+import { SET_POKEMONS, SET_FAVORITE } from "../actions/types";
 
 const initialState = fromJS({
   pokemons: [],
-  loading: false,
 });
 
 export const pokemonsReducer = (state = initialState, action) => {
   // se accede diferente con immutable 4.0.0
   switch (action.type) {
     case SET_POKEMONS:
-      // action.action.payload, porque uso un midleware q modifica el listado
-      // return setIn(state, ["pokemons"], fromJS(action.action.payload));
       return setIn(state, ["pokemons"], fromJS(action.payload));
-    case SET_LOADING:
-      return setIn(state, ["loading"], action.payload);
+
     case SET_FAVORITE:
       const currentPokemonIndex = get(state, "pokemons").findIndex(
         (pokemon) => pokemon.get("id") === action.payload.pokemonId
